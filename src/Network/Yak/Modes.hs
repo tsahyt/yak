ae26f11a0420cd3a583283f32d23f7f98183749e
Paul Ogris <paul@tsahyt.com>
2018-07-21 19:32:08 +0200

    Add documentation to Modes

--- src/Network/Yak/Modes.hs
+++ src/Network/Yak/Modes.hs
@@ -10,6 +10,10 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE PatternSynonyms #-}
 
+-- | A small DSL for creating mode strings. The documentation contains parts
+-- from <https://modern.ircdocs.horse> only for quick reference. Please refer to
+-- the source for up to date documentation on individual modes!
+
 module Network.Yak.Modes
     ( ModeStr
     , emitModeStr
@@ -21,6 +25,18 @@ module Network.Yak.Modes
     , ModeOp(..)
     , modeOps
     -- * Building Mode Strings
+    -- | Mode Strings can be built using the following three combinators. The
+    -- types are polymorphic over different mode types and therefore do not
+    -- fully reflect their use.
+    --
+    -- As an example consider
+    --
+    -- > foo :: ModeStr
+    -- > foo = set ban "foo!bar@quux" <> unset moderated
+    --
+    -- which translates to
+    --
+    -- > +b+m foo!bar@quux
     , get
     , set
     , unset
@@ -31,6 +47,7 @@ module Network.Yak.Modes
     , inviteOnly
     , inviteOnlyException
     , key
+    , secret
     , moderated
     , protectedTopic
     , noExternal
@@ -53,7 +70,10 @@ module Network.Yak.Modes
     -- * Server Modes
     , ServerModes(..)
     , defaultModes
-    , prefixModes
+    , defaultPrefixModes
+    , defaultUserModes
+    -- * Types
+    , HostMask
     ) where
 
 import Control.Applicative
@@ -76,6 +96,25 @@ data ModeType
     | TypeC
     | TypeD
 
+-- | A mode is defined through a character and its type.
+--
+--  * 'TypeA': Modes that add or remove an address to or from a list. These
+--  modes MUST always have a parameter when sent from the server to a client. A
+--  client MAY issue this type of mode without an argument to obtain the current
+--  contents of the list. The numerics used to retrieve contents of Type A modes
+--  depends on the specific mode. Also see the EXTBAN parameter.
+--
+--  * 'TypeB': Modes that change a setting on a channel. These modes MUST always
+--  have a parameter.
+--
+--  * 'TypeC': Modes that change a setting on a channel. These modes MUST have a
+--  parameter when being set, and MUST NOT have a parameter when being unset.
+--
+--  * 'TypeD': Modes that change a setting on a channel. These modes MUST NOT
+--  have a parameter.
+--
+--  The final type parameter to 'Mode' determines the parameter type of the
+--  defined mode.
 data Mode :: ModeType -> * -> * where
     ModeTypeA :: Char -> Mode 'TypeA a
     ModeTypeB :: Char -> Mode 'TypeB a
@@ -88,12 +127,21 @@ modeChar (ModeTypeB c) = c
 modeChar (ModeTypeC c) = c
 modeChar (ModeTypeD c) = c
 
+-- | An existential wrapper around modes that hides the parameter type, such
+-- that modes of a similar type can be grouped together in a simple container,
+-- such as those in a 'ServerModes'.
 data OpaqueMode (t :: ModeType) =
     forall a. ModeParameter a => OpaqueMode (Mode t a)
 
 matchOpaque :: Char -> OpaqueMode t -> Bool
 matchOpaque c (OpaqueMode m) = modeChar m == c
 
+-- | 'ServerModes' lets you define a dictionary of modes to be passed to the
+-- parser (see 'fetchModeStr'). Modes are very server specific and it is not
+-- possible to achieve good coverage/compatibility here, so this needs to be
+-- done in user code. See 'defaultModes' for a starting point. Note that
+-- 'ServerModes' is a monoid, so you can easily augment the standardized modes
+-- with custom server specific ones.
 data ServerModes = ServerModes
     { typeAModes :: [OpaqueMode 'TypeA]
     , typeBModes :: [OpaqueMode 'TypeB]
@@ -155,6 +203,9 @@ matchMode smodes f
             Getting _ -> fail "invalid modestring"
     | otherwise = empty
 
+-- | Fetch a 'ModeStr' from a 'ByteString', given some defined collection of
+-- modes in the form of a 'ServerModes'. Will return 'Nothing' if the parsing
+-- fails.
 fetchModeStr :: ServerModes -> ByteString -> Maybe ModeStr
 fetchModeStr smodes = either (const Nothing) Just . A.parseOnly go
   where
@@ -176,12 +227,14 @@ fetchModeStr smodes = either (const Nothing) Just . A.parseOnly go
                     ms
         pure $ map (matchMode smodes) cs
 
+-- | Emit a 'ModeStr' to a 'ByteString' for use in a message.
 emitModeStr :: ModeStr -> ByteString
 emitModeStr (ModeStr ms) =
     let cs = B.concat . toList . fmap opModeCmd $ ms
         ps = B.unwords . toList . fmap opModeParam $ ms
      in cs <> " " <> ps
 
+-- | Mode Parameters are types that can be used as parameters to some mode.
 class ModeParameter a where
     renderMode :: a -> ByteString
     seizeMode :: Parser a
@@ -207,7 +260,9 @@ instance ModeParameter Text where
                 then empty
                 else pure x
 
+-- | Operations that can be performed with a mode.
 data ModeOp where
+    -- | Type A modes can be retrieved in list form.
     GetMode :: Mode 'TypeA a -> ModeOp
     SetMode :: ModeParameter a => Mode t a -> Maybe a -> ModeOp
     UnsetMode :: ModeParameter a => Mode t a -> Maybe a -> ModeOp
@@ -270,30 +325,58 @@ instance UnsetMode (Mode 'TypeD Void) where
 
 type HostMask = Text
 
+-- | This channel mode controls a list of client masks that are ‘banned’ from
+-- joining or speaking in the channel. If this mode has values, each of these
+-- values should be a client mask.
 ban :: Mode 'TypeA HostMask
 ban = ModeTypeA 'b'
 
+-- | This channel mode controls a list of client masks that are exempt from the
+-- ‘ban’ channel mode. If this mode has values, each of these values should be a
+-- client mask.
 exception :: Mode 'TypeA HostMask
 exception = ModeTypeA 'e'
 
+-- | This channel mode controls whether new users may join based on the number
+-- of users who already exist in the channel. If this mode is set, its value is
+-- an integer and defines the limit of how many clients may be joined to the
+-- channel.
 clientLimit :: Mode 'TypeC Int
 clientLimit = ModeTypeC 'l'
 
+-- | This channel mode controls whether new users need to be invited to the
+-- channel before being able to join.
 inviteOnly :: Mode 'TypeD Void
 inviteOnly = ModeTypeD 'i'
 
+-- | This channel mode controls a list of channel masks that are exempt from the
+-- invite-only channel mode. If this mode has values, each of these values
+-- should be a client mask.
 inviteOnlyException :: Mode 'TypeA HostMask
 inviteOnlyException = ModeTypeA 'I'
 
+-- | This mode letter sets a ‘key’ that must be supplied in order to join this
+-- channel. If this mode is set, its’ value is the key that is required.
 key :: Mode 'TypeB Text
 key = ModeTypeB 'k'
 
+-- | This channel mode controls whether the channel is ‘secret’, and does not
+-- have any value.
+secret :: Mode 'TypeD Void
+secret = ModeTypeD 's'
+
+-- | This channel mode controls whether users may freely talk on the channel,
+-- and does not have any value.
 moderated :: Mode 'TypeD Void
 moderated = ModeTypeD 'm'
 
+-- | This channel mode controls whether channel privileges are required to set
+-- the topic, and does not have any value.
 protectedTopic :: Mode 'TypeD Void
 protectedTopic = ModeTypeD 't'
 
+-- | This channel mode controls whether users who are not joined to the channel
+-- can send messages to it, and does not have any value.
 noExternal :: Mode 'TypeD Void
 noExternal = ModeTypeD 'n'
 
@@ -302,18 +385,30 @@ type UserMode = Mode 'TypeD Void
 pattern UserMode :: Char -> UserMode
 pattern UserMode c = ModeTypeD c
 
+-- | If a user is set to ‘invisible’, they will not show up in commands such as
+-- WHO unless they share a channel with the user that submitted the command. In
+-- addition, the only channels that will show up in a WHOIS of an invisible user
+-- will be those they share with the user that submitted the command.
 invisible :: UserMode
 invisible = UserMode 'i'
 
+-- | If a user has this mode, this indicates that they are a network operator.
 oper :: UserMode
 oper = UserMode 'o'
 
+-- | If a user has this mode, this indicates that they are a server operator. A
+-- local operator has operator privileges for their server, and not for the rest
+-- of the network.
 localOper :: UserMode
 localOper = UserMode 'O'
 
+-- | If a user has this mode, this indicates that they have logged into a user
+-- account.
 registered :: UserMode
 registered = UserMode 'r'
 
+-- | If a user has this mode, this indicates that they will receive WALLOPS
+-- messages from the server.
 wallops :: UserMode
 wallops = UserMode 'w'
 
@@ -350,7 +445,18 @@ defaultModes =
           [ OpaqueMode moderated
           , OpaqueMode protectedTopic
           , OpaqueMode noExternal
-          , OpaqueMode invisible
+          , OpaqueMode secret
+          ]
+    }
+
+defaultUserModes :: ServerModes
+defaultUserModes =
+    ServerModes
+    { typeAModes = []
+    , typeBModes = []
+    , typeCModes = []
+    , typeDModes =
+          [ OpaqueMode invisible
           , OpaqueMode oper
           , OpaqueMode localOper
           , OpaqueMode registered
@@ -358,8 +464,8 @@ defaultModes =
           ]
     }
 
-prefixModes :: ServerModes
-prefixModes =
+defaultPrefixModes :: ServerModes
+defaultPrefixModes =
     ServerModes
     { typeAModes = []
     , typeBModes =
